35817ea9d29a3f82099e2b2bf5dfe2979de22c43
beardedprince <eizedomi@sbsc.com>
2021-10-15 10:18:55 +0100

    add service file

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ProductService {
 
-  _albumUrl= "../assets/album.json"
+  private _albumUrl= "../assets/album.json"
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
