269d2e6aeb9b16e90f3a41ad9fced0ec16b9cbd0
beardedprince <eizedomi@sbsc.com>
2021-10-15 12:26:20 +0100

    add router to products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,13 @@ import { ProductService } from './product.service';
 import { HttpModule } from '@angular/http';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  {path: 'products', component: ProductListComponent},
+  {path: 'product/:id', component: ProductPageComponent},
+  {path: '', redirectTo:'products', pathMatch: 'full'}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,6 +28,7 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes),
     HttpModule
   ],
   providers: [
