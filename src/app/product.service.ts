307631705e25334ca98715857d363394c8fd8055
Alaketu Olamilekan Soliu <30920382+beardedprince@users.noreply.github.com>
2021-10-30 15:13:35 +0100

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -19,10 +19,9 @@ export class ProductService {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 
-  getProducts(): Observable<Product[]> {
-    return this._http.get(this._productsUrl).pipe( map((response) => <Product[]>response.json())
-    );
-
+getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
   }
+
 
 }
